25e40da7f6d61d075def9d07a0a2739be35959ec
khuong30072001 <64116173+khuong30072001@users.noreply.github.com>
2023-04-08 22:45:58 +0700

    update auto speak + theme

--- android/app/src/main/kotlin/com/example/voicegpt/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.voicegpt
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const bgColor = Color(0xff303a80);
-const textColor = Color(0xffFEFDFC);
-const chatbgColor = Color(0xff4e5450);
